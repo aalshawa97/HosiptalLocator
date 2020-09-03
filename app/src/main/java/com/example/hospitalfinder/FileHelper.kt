7ff60d9e2a2370198fc3cdb94bc3e8113d2e8470
aalshawa97 <abdullahalshawa@gmail.com>
2020-09-03 16:10:15 -0700

    File Helper
    Reads files in JSON to find locations of hospitals

---
+++ app/src/main/java/com/example/hospitalfinder/FileHelper.kt
@@ -0,0 +1,14 @@
+//Abdullah Mutaz Alshawa
+//Description: reads json files to parse locations for hospitals
+package com.example.hospitalfinder
+import android.content.Context
+
+class FileHelper{
+    companion object{
+//        fun getTextFromResources(context: Context, resourceId Int){
+//            return context.resources.openRawResource(resourceId).use{
+//
+//       //     }
+       // }
+    }
+}
