3f71e238357e78c29457abbe9411edcc0b652ca1
aalshawa97 <abdullahalshawa@gmail.com>
2020-09-03 19:00:46 -0700

    Hospital Repository

---
+++ app/src/main/java/com/example/hospitalfinder/Global.kt
@@ -0,0 +1,3 @@
+package com.example.hospitalfinder
+
+const val LOG_TAG = "hospitalLogging"
---
+++ app/src/main/java/com/example/hospitalfinder/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.hospitalfinder
+
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+//class MainViewModel:ViewModel(app: Application) :AndroidView(app){
+//    // TODO: Implement the ViewModel
+//    init{
+//    val text = FileHelper.getTextFromResources(app, R.raw.hospital_data)
+//    Log.i(tag:LOG_TAG, text)
+
+    //fun parseText(text: String){
+//        val moshi = Moshi.Builder().build()
+//        val adapter:JsonAdapter<List<Monster>> = moshi.adapter(listType)
+//// }
+//}
