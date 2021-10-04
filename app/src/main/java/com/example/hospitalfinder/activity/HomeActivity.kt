b0da479013272a103e43a760b93836b68b35109e
aalshawa97 <abdullahalshawa@gmail.com>
2021-10-04 10:15:15 -0700

    Google Sign In

--- app/src/main/java/com/example/hospitalfinder/activity/HomeActivity.kt
+++ app/src/main/java/com/example/hospitalfinder/activity/HomeActivity.kt
@@ -8,13 +8,33 @@ import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.util.Log
 import android.widget.Toast
 import com.example.hospitalfinder.MapsActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import java.util.Timer
 import kotlin.concurrent.schedule
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+
+
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.hospitalfinder.R.layout.activity_home)
+        //Configure sign-in to request the user's ID, email address, and basic profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        //Configure sign-in to request the user's ID, email address, and basic profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        // Build a GoogleSignInClient with the options specified by gso.
+        var mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        //updateUI(account)
         Timer("SettingUp", false).schedule(3000) {
             val myIntent = Intent(this@HomeActivity, MapsActivity::class.java)
             this@HomeActivity.startActivity(myIntent)
