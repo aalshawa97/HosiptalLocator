ae259025a77dee0514c77d4ea4b9aee9cb1c0877
aalshawa97 <abdullahalshawa@gmail.com>
2021-10-18 10:55:36 -0700

    Test if application name is initialized!

--- app/src/test/java/com/example/hospitalfinder/LoginActivityTest.java
+++ app/src/test/java/com/example/hospitalfinder/LoginActivityTest.java
@@ -2,6 +2,7 @@ package com.example.hospitalfinder;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 
 import com.example.hospitalfinder.activity.LoginActivity;
@@ -17,5 +18,20 @@ public class LoginActivityTest {
     @Test(expected = ArithmeticException.class)
     public void testDivisionWithException() {
         int i = 1 / 0;
+    }
+
+    @Test
+    public void testAppNameInit() {
+        if(R.string.app_name != 0)
+        {
+            //Return true
+            assertEquals(0, 0);
+        }
+        else
+        {
+            //Return false
+            assertEquals(0, 1);
+        }
+
     }
 }
