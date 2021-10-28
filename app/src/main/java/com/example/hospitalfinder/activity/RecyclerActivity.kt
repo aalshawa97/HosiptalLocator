625d5e778a38b6944ed22214dfd07ce442f9d801
aalshawa97 <abdullahalshawa@gmail.com>
2021-10-28 10:59:48 -0700

    Phone call

--- app/src/main/java/com/example/hospitalfinder/activity/RecyclerActivity.kt
+++ app/src/main/java/com/example/hospitalfinder/activity/RecyclerActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -53,5 +54,9 @@ class RecyclerActivity : AppCompatActivity() {
 }
 
     fun onLogin(view: android.view.View) {}
-    fun makeCall(view: android.view.View) {}
+    fun makeCall(view: android.view.View) {
+        Toast.makeText(this, "Phone calling", Toast.LENGTH_LONG).show()
+        val intent = Intent(this, PhoneActivity::class.java)
+        startActivity(intent)
+    }
 }
