efdcc848eaa70f71344b3b25065a5ca96a91b22e
aalshawa97 <abdullahalshawa@gmail.com>
2021-10-29 15:13:32 -0700

    Display map activity

--- app/src/main/java/com/example/hospitalfinder/activity/ShowHospitalsActivity.kt
+++ app/src/main/java/com/example/hospitalfinder/activity/ShowHospitalsActivity.kt
@@ -2,11 +2,9 @@ package com.example.hospitalfinder.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.hospitalfinder.R
 
-class DisplayMapActivity : AppCompatActivity() {
+class ShowHospitalsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_hospitals)
     }
 }
